ce08e0e84207e0dbf4ec7a4cf892706b63292613
unknown <jair.lopes@fatec.sp.gov.br>
2022-01-08 17:27:47 -0300

    Iniciado curso

---
+++ Aula12 - Partindo para Modelagem Física/universidade_u_fisica.sql
@@ -0,0 +1,12 @@
+/* universidade_u_logica: */
+
+CREATE TABLE ALUNO (
+    sexo char(1),
+    idade int,
+    endereco text,
+    nome varchar(25),
+    telefone varchar(20),
+    valor_pago_curso float(10, 2),
+    data_inscricao_curso date,
+    ativo_sn int
+);
